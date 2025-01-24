b91e82a52245d64ca40b5a3a719d3a20ec41ff70
pohlrabi <pohlrabi404@gmail.com>
2025-01-24 15:53:39 +0900

    change useradd to chroot instead

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -58,3 +58,16 @@ EOF
 # set password
 systemctl enable NetworkManager
 passwd
+
+useradd -m pohlrabi
+passwd pohlrabi
+
+cat <<EOF > /etc/sudoers
+root ALL=(ALL:ALL) ALL
+
+Defaults targetpw
+ALL ALL=(ALL:ALL) ALL
+EOF
+
+# Security
+sudo echo "auth optional pam_faildelay.so delay=4000000" >> /etc/pam.d/system-login
--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,13 +1,1 @@
 pacman -S sudo vim --noconfirm
-useradd -m pohlrabi
-passwd pohlrabi
-
-cat <<EOF > /etc/sudoers
-root ALL=(ALL:ALL) ALL
-
-Defaults targetpw
-ALL ALL=(ALL:ALL) ALL
-EOF
-
-# Security
-sudo echo "auth optional pam_faildelay.so delay=4000000" >> /etc/pam.d/system-login
