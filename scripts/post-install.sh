95057474cce8f95b144678f81e7a6fc2ea79cd7a
pohlrabi <pohlrabi404@gmail.com>
2025-01-24 16:02:45 +0900

    add rebos

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,1 +1,8 @@
 sudo pacman -S sudo vim --noconfirm
+cp /etc/pacman.conf tmp
+cat <<EOF > tmp
+[oglo-arch-repo]
+SigLeel = Optional DatabaseOptional
+Server = https://gitlab.com/Oglo12/$repo/-/raw/main/$arch
+EOF
+sudo mv tmp /etc/pacman.conf
