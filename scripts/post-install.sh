a3220b48f089b3edb655365bcd8932b8db7977a0
pohlrabi <pohlrabi404@gmail.com>
2025-01-24 17:28:28 +0900

    setup rebos

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,4 +1,5 @@
-sudo pacman -S sudo vim --noconfirm
+set -e
+sudo pacman -S vim --noconfirm
 
 # set up rebos
 cp /etc/pacman.conf tmp
@@ -9,4 +10,13 @@ Server = https://gitlab.com/Oglo12/$repo/-/raw/main/$arch
 EOF
 sudo mv tmp /etc/pacman.conf
 sudo pacman -Syy
-sudo pacman -S rebos paru stow --noconfirm
+sudo pacman -S rebos paru stow openssh --noconfirm
+
+git clone https://github.com/pohlrabi404/arch-scripts ~/scripts
+cd ~/scripts/dotfiles
+stow -v -t ~ .
+cd ~
+rebos setup
+rebos config init
+rebos gen commit "init commit"
+rebos gen current build
