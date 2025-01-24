36edf884448cf653db26d9eade2c95c03671148a
pohlrabi <pohlrabi404@gmail.com>
2025-01-24 17:31:31 +0900

    bugs squash

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -6,7 +6,7 @@ cp /etc/pacman.conf tmp
 cat <<EOF >> tmp
 [oglo-arch-repo]
 SigLeel = Optional DatabaseOptional
-Server = https://gitlab.com/Oglo12/$repo/-/raw/main/$arch
+Server = https://gitlab.com/Oglo12/\$repo/-/raw/main/\$arch
 EOF
 sudo mv tmp /etc/pacman.conf
 sudo pacman -Syy
